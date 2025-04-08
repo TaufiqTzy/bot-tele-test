09c877056d71ad0a55cf26f11ec2e8431759e755
Muhammad Taufiqurrahman <m.taufiqurrahman2912@gmail.com>
2025-04-08 11:41:14 +0700

    komit

---
+++ resources/views/botman/chat.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Chat</title>
+</head>
+<body>
+    <h1>BotMan Web Widget</h1>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use BotMan\BotMan\BotMan;
+use BotMan\BotMan\Messages\Incoming\Answer;
+use BotMan\BotMan\BotManFactory;
+use BotMan\BotMan\Drivers\DriverManager;
 
+use BotMan\BotMan\Cache\LaravelCache;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +18,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::match(['get', 'post'], '/botman', function () {
+    DriverManager::loadDriver(\BotMan\Drivers\Web\WebDriver::class);
+
+    $config = [];
+
+    $botman = BotManFactory::create($config, new LaravelCache());
+
+    $botman->hears('hi|hello', function (BotMan $bot) {
+        $bot->reply('Hello! How can I help you?');
+    });
+
+    $botman->listen();
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::view('/botman/chat', 'botman.chat');
