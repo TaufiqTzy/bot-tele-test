8836f9de47e8e2809b31bb5e1ec2c57cb9644f46
Muhammad Taufiqurrahman <113502696+TaufiqTzy@users.noreply.github.com>
2025-04-08 06:46:31 +0000

    anjay

--- resources/views/botman/chat.blade.php
+++ resources/views/botman/chat.blade.php
@@ -2,9 +2,18 @@
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>Chat</title>
+    <title>BotMan Chat Frame</title>
 </head>
 <body>
-    <h1>Loading chat...</h1>
+
+    <script>
+        var botmanWidget = {
+            chatServer: '/botman',
+            frameEndpoint: null,
+        };
+    </script>
+
+    <script src='https://cdn.jsdelivr.net/npm/botman-web-widget@0/build/js/chat.js'></script>
+
 </body>
 </html>
